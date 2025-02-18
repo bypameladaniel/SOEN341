65e07eb0cd65d5db1dadc3b0965759437e4a8cd3
amal-1224 <amalsabsabi@icloud.com>
2025-02-17 21:55:08 -0500

    - Adding Lucide-react node modules (to use icons) - Creating the Login/SignUp/ForgotPassword/ResetPassword components (their respective .tsx and .css files) - Writing the form submission handling logic for when the user logs in or signs up (managing the state ONLY FRONTEND) - Issues #23

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,25 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginSignup from "./components/login-signup/LoginSignup"
+import ResetPassword from "./components/password/ResetPassword"
+import ForgotPassword from "./components/password/ForgotPassword"
+import Layout from "./components/layout";
+
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}> //Mia is doing the layout?
+        <Route index element={<LoginSignup/>}/>
+        <Route path="forgotpassword" element={<ForgotPassword/>}/>
+        <Route path="resetpassword" element={<ResetPassword/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App/>);
 export default App
