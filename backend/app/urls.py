e048dae3905f6af2ba84848b1f3125d229194545
bypameladaniel <pamelaelidaniel@gmail.com>
2025-02-22 17:05:10 -0500

    Issue #37: Adding the Custom User Model for authentication

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 from .views import ReactAppView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     #path('', views.home, name='home'),
     path('', ReactAppView.as_view(), name='react_app'),
     # Add other URL patterns here
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
