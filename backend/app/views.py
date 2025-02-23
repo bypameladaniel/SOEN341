d8c2983edf40975660e3ede96e98b3303797cc75
Soukay55 <haitamis@hotmail.com>
2025-02-23 13:09:32 -0500

    Issue #39 : Added the authentication backend that takes on username and password

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from rest_framework.decorators import api_view
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from allauth.socialaccount.models import SocialToken, SocialAccount
+from django.contrib.auth.decorators import login_required
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-@api_view(['GET'])
-def api_example(request):
-    data = {'message': 'Hello from Django!'}
-    return Response(data)
+#@api_view(['GET'])
+#def api_example(request):
+    #data = {'message': 'Hello from Django!'}
+    #return Response(data)
 
 class ReactAppView(TemplateView):
     template_name = 'index.html'  # Ensure this points to your React index.html
@@ -16,6 +28,19 @@ class ReactAppView(TemplateView):
         context = super().get_context_data(**kwargs)
         # Add any additional context data here
         return context
+    
+User = get_user_model()
 
-#def home(request):
-    #return HttpResponse("Hello World")
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class UserDetailView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from app.views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("app/user/register", UserCreate.as_view(), name ="user_create"),
+    path("app/token/",TokenObtainPairView.as_view(), name = "token_refresh"),
+    path("app-auth/",include('rest_framework.urls')),
+    path("accounts/",include("allauth.urls")),
+    path("app/auth/user/",UserDetailView.as_view(), name = "user_detail"),
     path("",include('app.urls')),
 ]
