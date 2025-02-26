c59d93859d64695ebb976f64cfe080a146475061
wisoums <wissem_oumsalem@yahoo.ca>
2025-02-26 17:06:32 -0500

    Removed path('join_channel/', join_channel, name='join_channel')

--- backend/channels/urls.py
+++ backend/channels/urls.py
@@ -7,7 +7,6 @@ from .views import join_channel
 urlpatterns = [
     #path('channel-list/', channel_list, name='channel-list'),
     path('', channel_list, name='channel-list'),
-    path('join/<int:channel_id>/', views.join_channel, name='join_channel'),
-    path('join_channel/', join_channel, name='join_channel'),
+    path('join/<int:channel_id>/', views.join_channel, name='join-channel'),
 
 ]
