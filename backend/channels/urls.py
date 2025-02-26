82492169092a1a4894302fa4eb55838a13c7124e
bypameladaniel <pamelaelidaniel@gmail.com>
2025-02-26 15:23:09 -0500

    Issue #70: Listing Messages Endpoint

--- backend/channels/urls.py
+++ backend/channels/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import channel_list
-
+from .views import channel_list, list_messages_in_channel
+    
+#every url defined in this folder will AUTOMATICALLY start with /channels 
 urlpatterns = [
     #path('channel-list/', channel_list, name='channel-list'),  
-    path('', channel_list, name='channel-list'),  
+    path('', channel_list, name='channel-list'),
+    path('<int:channel_id>/messages/', list_messages_in_channel, name='message-list')  
+
 ]
--- backend/channels/views.py
+++ backend/channels/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.conf import settings
-from .models import Channel
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Channel, Message
+from .serializers import MessageSerializer
 
 def channel_list(request):
     if request.method == "GET":
@@ -20,3 +23,19 @@ def channel_list(request):
         return JsonResponse(data, safe=False)
 
     return JsonResponse({"error": "Method not allowed"}, status=405)
+
+@login_required
+def list_messages_in_channel(request, channel_id):
+    if request.method == 'GET':
+        try:
+            # If the Channel does not exist, the user should be redirected to a 404 page
+            channel = Channel.objects.get(id=channel_id)
+        except Channel.DoesNotExist:
+            return JsonResponse({'error': 'Channel not found'}, status=404)
+
+        messages = Message.objects.filter(channel=channel).order_by('timestamp')
+
+        serializer = MessageSerializer(messages, many=True)
+        return JsonResponse({'messages': serializer.data}, status=200)
+    else:
+        return JsonResponse({"error": "Method not allowed"}, status=405)
