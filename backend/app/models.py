797706d5dec59405be4e69f5bfbd68ac22faa349
wisoums <wissem_oumsalem@yahoo.ca>
2025-02-28 15:30:16 -0500

    Issue #100: Add a method to check if a user is an admin

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,10 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    # AbstractUser class has username and password fields as required
     POSSIBLE_ROLES = (
         ('admin', 'Admin'),
         ('member', 'Member'),
     )
     role = models.CharField(max_length=6, choices=POSSIBLE_ROLES, default='member')
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+    def is_admin(self):
+        return self.role == 'admin'
