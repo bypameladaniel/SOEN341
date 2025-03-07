10096103f386f058d0f7cc3958880f764aff615d
Pamela Daniel <149453946+bypameladaniel@users.noreply.github.com>
2025-03-07 12:42:49 -0500

    Issue #91: Add some admin code so that admin dev can see the models visually

--- backend/direct_messages/admin.py
+++ backend/direct_messages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import DirectMessage
 
-# Register your models here.
+@admin.register(DirectMessage)
+class DirectMessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'message', 'timestamp')
+    search_fields = ('sender__username', 'receiver__username', 'message')
+    ordering = ('-timestamp',)
