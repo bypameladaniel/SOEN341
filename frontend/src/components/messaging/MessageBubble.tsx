4eee901e26dacceee647b2d82230d7a101b9c200
catalexandre <catellier.alexandre@gmail.com>
2025-03-19 12:46:25 -0400

    Added the name indicator of who sent what message for issue 149

--- frontend/src/components/messaging/MessageBubble.tsx
+++ frontend/src/components/messaging/MessageBubble.tsx
@@ -4,19 +4,21 @@ import './MessageBubble.css'
 interface MessageBubbleProps {
     message: string;
     sender?: boolean;
+    senderName?: string; // Add senderName prop
 }
 
-const MessageBubble: React.FC<MessageBubbleProps> = ({ message, sender }) => {
+const MessageBubble: React.FC<MessageBubbleProps> = ({ message, sender, senderName }) => {
     return (
         <div className={`Bubble test ${sender ? "sender" : "receiver"}`}> 
+            {!sender && senderName && <div className="sender-name">{senderName}</div>} {/* Display sender name if it's a received message */}
             {message.split('\n').map((line, index) => (
-        <React.Fragment key={index}>
-          {line}
-          <br />
-        </React.Fragment>
-      ))}
+                <React.Fragment key={index}>
+                    {line}
+                    <br />
+                </React.Fragment>
+            ))}
         </div>
     )
 }
 
-export default MessageBubble
+export default MessageBubble;
