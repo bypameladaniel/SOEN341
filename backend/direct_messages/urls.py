a0d59196c647cc44353a590abcdb568b5d8b8f72
catalexandre <catellier.alexandre@gmail.com>
2025-03-20 12:39:49 -0400

    Revert "Merge pull request #133 from bypameladaniel/122-implement-api-endpoint-send-messages"
    This reverts commit 6d0111c6ceaa2e98eb8be23f4ff73a11fa440c75, reversing
    changes made to 53afbec68fb0c4cecd37e05c5d7a7ae917201ecf.

--- backend/direct_messages/urls.py
+++ backend/direct_messages/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
-from .views import UserListView
-from .views import SendMessageView
-
-# Note that the path start with 'api/direct_messages/'
+from .views import list_direct_messages
+from . import views
+    
 urlpatterns = [
-    path('list-users/', UserListView.as_view(), name='user-list'),
-    path('send-message/', SendMessageView.as_view(), name='send-message'),
+    path('<int:user_id>/', list_direct_messages, name='list-direct-messages'),
 ]
+
 
--- backend/direct_messages/views.py
+++ backend/direct_messages/views.py
@@ -1,35 +1,26 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
-from app.serializers import UserSerializer  
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
 from .models import DirectMessage
 from .serializers import DirectMessageSerializer
 
-from django.shortcuts import get_object_or_404
-from .models import DirectMessage
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
 
-User = get_user_model()
-
-class UserListView(generics.ListAPIView):
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        current_user = self.request.user
-        return User.objects.exclude(id=current_user.id)
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def list_direct_messages(request, user_id):
+    User = get_user_model()
+    other_user = get_object_or_404(User, id=user_id)
     
-class SendMessageView(generics.CreateAPIView):
-    queryset = DirectMessage.objects.all()
-    serializer_class = DirectMessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    current_user = request.user
+    messages = DirectMessage.objects.filter(
+        Q(sender=current_user, receiver=other_user) | Q(sender=other_user, receiver=current_user)
+    ).order_by('timestamp')
 
-    def perform_create(self, serializer):
-        receiver = self.request.data.get("receiver")  
-        if not receiver:
-            raise serializers.ValidationError({"receiver": "This field is required."})
-        
-        receiver = get_object_or_404(User, id=receiver)
-        
-        serializer.save(sender=self.request.user, receiver=receiver)
+    serializer = DirectMessageSerializer(messages, many=True, context={"request": request})
+    return Response({'direct_messages': serializer.data}, status=status.HTTP_200_OK)
