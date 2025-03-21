5e051a8df2b146ab3d8220abb729f1d7877cf6e1
Soukay55 <haitamis@hotmail.com>
2025-03-21 01:27:12 -0400

    #125 : Implemented the list conversations endpoint

--- backend/direct_messages/urls.py
+++ backend/direct_messages/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from .views import UserListView
 from .views import SendMessageView
-
+from .views import ConversationView
+from .views import list_conversations
 # Note that the path start with 'api/direct_messages/'
 urlpatterns = [
     path('list-users/', UserListView.as_view(), name='user-list'),
     path('send-message/', SendMessageView.as_view(), name='send-message'),
-]
+    path('conversation/<int:recipient_id>/', ConversationView.as_view(), name='conversation'),
+    path('conversations/', list_conversations, name='list_conversations'),
 
+]
