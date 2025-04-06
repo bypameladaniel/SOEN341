8c9d4289ef985aded7c8a6e51b2d9f49c303fc2f
amal-1224 <amalsabsabi@icloud.com>
2025-04-05 22:45:06 -0400

    Issue #203 #204: fix issue causing tests to fail

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 from .models import User
 from django.contrib.auth.password_validation import validate_password
